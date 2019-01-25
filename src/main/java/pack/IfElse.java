8e77dcedf8f08b8c3034938ba432bfe66109b73c
preetis <preeti.singh136@gmail.com>
2019-01-25 15:22:03 +0530

    Java Learning

---
+++ src/main/java/pack/IfElse.java
@@ -0,0 +1,25 @@
+package pack;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2 == 1) {
+            System.out.println("Weird");
+        }
+        else {
+            if(n<5 && n>2){
+                System.out.println("Not Weird");
+            }
+            else if(n<20 &&n>6){
+                System.out.println("Weird");
+            }
+            else{
+                System.out.println("Not Weird");
+            }
+        }
+
+    }
+}
